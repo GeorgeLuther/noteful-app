e51156165a6a315db2cecc61d407a33db5a3bb0a
George Luther <george_l@thinkful.com>
2021-01-14 14:42:01 -0500

    changed base url again

--- src/GlobalFuncs.js
+++ src/GlobalFuncs.js
@@ -1,6 +1,6 @@
 // get the notes/folders from state via context
 
-export const BASE_URL = 'https://mighty-headland-60110.herokuapp.com/'
+export const BASE_URL = 'https://mighty-headland-60110.herokuapp.com'
 
 export const findFolder = (folders=[], folderId) =>  folders.find(folder => folder.id === folderId)
 
